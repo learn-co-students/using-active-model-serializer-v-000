a26c3b2511aced8f6ef4b376d20b1eb57c71a217
Sophia Hubeny <shhubeny@gmail.com>
2017-12-05 10:58:40 +0000

    Done.

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,3 +1,7 @@
 class PostSerializer < ActiveModel::Serializer
   attributes :id, :title, :description
+  belongs_to :author, serializer: PostAuthorSerializer
+   #associations here will let Rails know what needs to go in the JSON
+   #explicitly render PostAuthorSerializer instead of relying on convention - Rails knows to just render author
+   #with PostAuthorSerializer
 end
