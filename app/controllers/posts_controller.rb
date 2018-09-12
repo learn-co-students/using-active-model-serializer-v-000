3361b6e4e554db8a40206bd94b0821e8d2fd360e
Victoria Meng <nights1ststar@protonmail.com>
2018-09-12 02:28:23 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,11 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
-    render json: @post
+    respond_to do |format|
+      format.html {render :show}
+      format.json {render json: @post}
+    end
+
   end
 
   def new
