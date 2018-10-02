a4d46f3fdb0a7ee0c58c0b4fca026fed5f4e99dc
johnwojo <jwojo13@gmail.com>
2018-10-02 17:02:43 -0400

    Done.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,34 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+    respond_to do |format|
+      format.html { :index}
+      format.json { render json: @products, status: 200}
+    end
+  end
+   def inventory
+     product = Product.find(params[:id])
+     render plain: product.inventory > 0 ? true : false
+   end
+
+   def description
+     product = Product.find(params[:id])
+     render plain: product.description
+   end
+
+   def new
+     @product = Product.new
+   end
+
+   def create
+     Product.create(product_params)
+     redirect_to products_path
+   end
+
+  def show
+    @product = Product.find(params[:id])
+    respond_to do |format|
+      format.html { :show }
+      format.json { render json: @product, status: 200}
+    end
+  end
