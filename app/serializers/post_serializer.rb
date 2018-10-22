0ed6bb553b79ca2dfb71268e315864715ebc240c
Jul Jen <juliajen.prog@gmail.com>
2018-10-22 18:23:14 +0000

    Done.

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,3 +1,6 @@
 class PostSerializer < ActiveModel::Serializer
   attributes :id, :title, :description
+  belongs_to :author, serializer: PostAuthorSerializer
+  #explicitly give it a serializer to use rather than relying on the convention
+  #AMS to render :author with PostAuthorSerializer instead of the default.
 end
