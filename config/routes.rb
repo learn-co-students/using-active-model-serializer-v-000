517108fd90e00f9dbd09dcc68f4353dd32ed32b7
Jul Jen <juliajen.prog@gmail.com>
2018-10-22 18:32:49 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  # root 'posts#index'
-
   resources :authors, only: [:show]
   resources :posts, only: [:index, :show, :new, :create, :edit]
-  
+
   get 'posts/:id/post_data', to: 'posts#post_data'
 end
