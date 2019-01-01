0d586563e93def47a93f97138dd04c3fffcf18de
amckean12 <amckean@me.com>
2019-01-01 09:15:34 -0600

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,6 +1,7 @@
 class AuthorsController < ApplicationController
   def show
     @author = Author.find(params[:id])
+    #Found in author_serializer
     render json: @author, status: 200
   end
 end
