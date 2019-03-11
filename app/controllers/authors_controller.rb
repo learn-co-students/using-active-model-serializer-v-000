04db583cb3a9074a40e758a2f42f1651e83f7017
freeWillee <will.ik.lee@gmail.com>
2019-03-11 15:08:46 -0600

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,6 +1,13 @@
 class AuthorsController < ApplicationController
   def show
     @author = Author.find(params[:id])
-    render json: @author, status: 200
+      #BEFORE using a serializer:
+      # render json: @post.to_json(only: [:title, :description, :id],
+      #                           include: [author: { only: [:name]}])
+       # AFTER USING OUR SERIALIZER
+      respond_to do |format|
+        format.html { render :show }
+        format.json { render json: @author, status: 200 }
+      end
   end
 end
