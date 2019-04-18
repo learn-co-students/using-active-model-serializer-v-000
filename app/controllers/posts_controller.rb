0a30d8010bd0ce2b78a3a8c3cd7f8ba57fa2a9b0
Mika88 <mikaspivak@gmail.com>
2019-04-18 17:20:13 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,11 +8,7 @@ class PostsController < ApplicationController
 
   def show
     @post = set_post
-      #BEFORE using a serializer:
-      # render json: @post.to_json(only: [:title, :description, :id],
-      #                           include: [author: { only: [:name]}])
-       # AFTER USING OUR SERIALIZER
-       render json: @post, status: 200
+    render json: @post, status: 200
   end
 
   def new
--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,4 +1,8 @@
 class PostSerializer < ActiveModel::Serializer
+<<<<<<< HEAD
   attributes :id, :title, :description
   belongs_to :author, serializer: PostAuthorSerializer
+=======
+   attributes :id, :title, :description
+>>>>>>> e5dd20d77800068513eaa9524434918eb755db5c
 end
